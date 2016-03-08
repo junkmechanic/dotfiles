981efaf3a2084a8e294b56ebcadea5e80e4a4f42
ankur <khanna-a@i2r.a-star.edu.sg>
2016-03-08 17:40:20 +0800

    vim: mappings for qf, python, slimux, vimperator colorscheme change, more utilities

--- util/utilities.py
+++ util/utilities.py
@@ -1,5 +1,7 @@
 import os
+import json
 import codecs
+from collections import namedtuple
 # import traceback as tb
 
 NEW_LINE = '\n'
@@ -55,3 +57,53 @@ class FileWriteBuffer:
             writeToFile(self.filename, (self.delim).join(
                 self.Buffer) + self.delim)
             self.Buffer = []
+
+
+def cleanupToken(token, reps_allowed=1):
+    """
+    reps_allowed : no. of repretitions allowed. For example:
+        'sloooow' -> 'slow' for reps_allowed = 0
+        'sloooow' -> 'sloow' for reps_allowed = 1
+    """
+    Extra = namedtuple('Extra', ['index', 'times'])
+    extra_occurences = []
+    prev = ''
+    counter = 0
+    for i in range(len(token)):
+        if token[i] == prev:
+            counter += 1
+        else:
+            if counter > reps_allowed:
+                pos = i - (counter - reps_allowed)
+                extra = counter - reps_allowed
+                extra_occurences.append(Extra(index=pos, times=extra))
+            counter = 0
+            prev = token[i]
+    if counter > reps_allowed:
+        pos = i - (counter - reps_allowed)
+        extra = counter - reps_allowed
+        extra_occurences.append(Extra(index=pos, times=extra))
+    new_token = list(token)
+    deleted = 0
+    for extra in extra_occurences:
+        for _ in range(extra.times):
+            del new_token[extra.index - deleted]
+        deleted += extra.times
+    return ''.join(new_token)
+
+
+def loadJSON(filename):
+    with open(filename) as ifi:
+        json_tree = json.load(ifi)
+    return json_tree
+
+
+def loadAllLines(filename):
+    with open(filename) as ifi:
+        all_lines = ifi.readlines()
+    return all_lines
+
+
+def saveJSON(obj, filename):
+    with open(filename, 'w') as ofi:
+        json.dump(obj, ofi)
