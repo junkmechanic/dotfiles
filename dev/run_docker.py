daff80b8a29bd5176bfbf78c3ab777caada6143a
ankur <ankur.khanna@grab.com>
2017-11-19 14:59:08 +0800

    updated docker & karabiner config; hyperterm config

---
+++ dev/run_docker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import docker
+
+# This is a translation of the following shell command
+# `docker run -d --init -p 8989:8888 \
+#    -v /host/dir/to/mount:/container/dir/to/mount/to --name dsnb dsnb_db.img`
+
+client = docker.from_env()
+
+client.containers.run(
+    image='dsnb_db.img',
+    detach=True,
+    init=True,
+    ports={'8888/tcp': 8989},
+    volumes={
+        '/home/ankur/devbench/notebooks/': {
+            'bind': '/home/ankur/notebooks',
+            'mode': 'rw'
+        },
+    },
+    name='dsnb',
+    # mem_limit='100g'
+)
