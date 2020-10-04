9209490aa494fc48e847134ca255c8fe04719935
junkmechanic <khanna89ankur@gmail.com>
2020-10-04 19:47:05 +0100

    fixed after virtualenv

--- etc/remote/pyenv.d/virtualenv/after.bash
+++ etc/remote/pyenv.d/virtualenv/after.bash
@@ -1,1 +1,5 @@
-after_virtualenv 'pip install --upgrade pip neovim neovim-remote ipython'
+upgrade_packages() {
+  PYENV_VERSION=$VIRTUALENV_NAME pyenv-exec pip install --upgrade pip setuptools wheel neovim neovim-remote ipython
+}
+
+after_virtualenv 'upgrade_packages'
