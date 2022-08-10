99feb060911b86d1347d1d7fd06e685ffc5df299
junkmechanic <khanna89ankur@gmail.com>
2022-08-10 19:50:33 +0100

    Improved diagnostics with mappings

--- etc/home/nvim/lua/config/lsp.lua
+++ etc/home/nvim/lua/config/lsp.lua
@@ -2,7 +2,18 @@ require('nvim-lsp-installer').setup {
   automatic_installation = true,
 }
 
+-- global diagnostics options
+vim.diagnostic.config { virtual_text = false }
+
+-- diagnostics symbols
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 local opts = { noremap = true, silent = true }
+vim.keymap.set('n', 'gt', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
