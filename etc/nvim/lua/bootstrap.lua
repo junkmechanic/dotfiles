de748ac35ee51b458c27cc0936a26b529b1b9ddd
Ankur Khanna <ankur.khanna@complyadvantage.com>
2024-12-07 19:18:45 +0000

    updated fzf config, suppressed treesitter lsp warning, bootstrap cleanup

--- etc/nvim/lua/bootstrap.lua
+++ etc/nvim/lua/bootstrap.lua
@@ -3,11 +3,3 @@ vim.filetype.add {
     ['requirements*.txt'] = 'config',
   },
 }
-
-function _G.debug_notif(arg)
-  local res = vim.inspect(arg)
-  local ok = pcall(require 'notify', res)
-  if not ok then
-    vim.notify(res)
-  end
-end
